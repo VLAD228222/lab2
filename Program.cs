51c4dc280eeb793edeadca8a34001affde932d1d
VLAD228222 <zxc710962@gmail.com>
2025-03-30 15:07:18 +0300

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,38 +1,32 @@
-﻿using System;
+using System;
 
 class Program
 {
     static void Main()
-    {
-        Console.Title = "Дейко Влад"; 
+    {  
+        Console.WriteLine($"Дейко Влад");
 
-        // Введення значень з клавіатури
-        Console.Write("Введіть x: ");
+        Console.Write("Введіть значення x: ");
         double x = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Введіть y: ");
+        Console.Write("Введіть значення y: ");
         double y = Convert.ToDouble(Console.ReadLine());
 
-        // Задання константних значень
-        double z = 0.88;
-        double a = 0.85;
-        double b = 0.56;
+        double z = 1.13;
+        double a = 0.25;
+        double b = 0.8;
 
-        // Обчислення значень виразів
-        double L = Math.Sin(Math.Atan(1 / z));
-        double m = (4 * Math.Pow(y, 2) * Math.Exp(Math.Pow(Math.Sin(x), 3))) / (3 * Math.Pow(z, 3) + Math.Log(x));
-        double j = (Math.Pow(Math.Log(x), 2) + Math.Sqrt(x + a)) / Math.Tan(b * x);
-        double q = Math.Sin(b * x) / (Math.Pow(b, 2) + Math.Pow(x, 2));
+        double m = Math.Sqrt(a + b * z) / Math.Pow(Math.Log(x), 2);
+        double f = Math.Pow(a, 2) * Math.Exp(-b * x);
+        double i = 1 + x + Math.Pow(x, 4) / y;
+        double j = x * Math.Atan(a);
 
-        // Виведення результатів
-        Console.WriteLine("\nРезультати обчислень:");
-        Console.WriteLine($"L = {L:F4}");
+        Console.WriteLine($"\nРезультати обчислень:");
         Console.WriteLine($"m = {m:F4}");
+        Console.WriteLine($"f = {f:F4}");
+        Console.WriteLine($"i = {i:F4}");
         Console.WriteLine($"j = {j:F4}");
-        Console.WriteLine($"q = {q:F4}");
 
-        // Очікування натискання клавіші перед виходом
-        Console.WriteLine("\nНатисніть будь-яку клавішу для виходу...");
-        Console.ReadKey();
+        Console.ReadLine();
     }
 }
